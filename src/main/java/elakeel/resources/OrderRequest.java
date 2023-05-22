99e0a043f354478c982bd4a49cf9f7c0541974e7
amahdyy <119822536+amahdyy@users.noreply.github.com>
2023-05-23 00:04:00 +0300

    Update OrderRequest.java

--- src/main/java/elakeel/resources/OrderRequest.java
+++ src/main/java/elakeel/resources/OrderRequest.java
@@ -3,3 +3,24 @@ package elakeel.resources;
 import java.util.List;
 
 import elakeel.ejbs.OrderItem;
+
+public class OrderRequest {
+    private Long restaurantId;
+    private List<OrderItem> items;
+
+    public Long getRestaurantId() {
+        return restaurantId;
+    }
+
+    public void setRestaurantId(Long restaurantId) {
+        this.restaurantId = restaurantId;
+    }
+
+    public List<OrderItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<OrderItem> items) {
+        this.items = items;
+    }
+}
