ce229fd6a3b32189e6ff56cc6e8947de35ee4f9d
AmroAdel1 <119890404+AmroAdel1@users.noreply.github.com>
2023-05-23 00:06:43 +0300

    Update RestaurantOwner.java

--- src/main/java/elakeel/ejbs/RestaurantOwner.java
+++ src/main/java/elakeel/ejbs/RestaurantOwner.java
@@ -4,3 +4,9 @@ import java.io.Serializable;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+
+@Entity
+@Table(name = "restaurant_owners")
+public class RestaurantOwner extends User implements Serializable {
+
+}
