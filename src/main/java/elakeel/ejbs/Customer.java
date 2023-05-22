54475847c2bc7da25777779715c22f15564cb2d6
mohamedsalama1020 <94934576+mohamedsalama1020@users.noreply.github.com>
2023-05-22 23:57:41 +0300

    Update Customer.java

--- src/main/java/elakeel/ejbs/Customer.java
+++ src/main/java/elakeel/ejbs/Customer.java
@@ -7,3 +7,24 @@ import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+
+@Entity
+@Table(name = "custmers")
+public class Customer extends User  {
+	
+	 @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+	    private List<Order> orders;
+	 
+	 public void addOrder(Order orderr) {
+			this.orders.add(orderr);
+			}
+
+	public List<Order> getOrders() {
+		return orders;
+	}
+
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
+	}
+	 
+}
