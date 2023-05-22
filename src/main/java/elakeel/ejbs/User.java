8ebb20f7f0003c5de2738a8b07223a4a8b9443ef
AmroAdel1 <119890404+AmroAdel1@users.noreply.github.com>
2023-05-23 00:11:57 +0300

    Update User.java

--- src/main/java/elakeel/ejbs/User.java
+++ src/main/java/elakeel/ejbs/User.java
@@ -11,3 +11,55 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+    public User() {
+		super();
+	}
+
+	public User(Long id, String name, UserRole role) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.role = role;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public UserRole getRole() {
+		return role;
+	}
+
+	public void setRole(UserRole role) {
+		this.role = role;
+	}
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+   
+}
