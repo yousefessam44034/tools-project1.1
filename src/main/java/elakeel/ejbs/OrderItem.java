ea54bc4cb9223b8c2834300801571bb5828c282f
amahdyy <119822536+amahdyy@users.noreply.github.com>
2023-05-22 23:58:29 +0300

    Update OrderItem.java

--- src/main/java/elakeel/ejbs/OrderItem.java
+++ src/main/java/elakeel/ejbs/OrderItem.java
@@ -8,3 +8,26 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+
+
+public class OrderItem implements Serializable {
+    public OrderItem() {
+		super();
+	}
+
+	public OrderItem(Long id, Order order, Meal meal) {
+		super();
+		this.id = id;
+		this.order = order;
+		this.meal = meal;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
