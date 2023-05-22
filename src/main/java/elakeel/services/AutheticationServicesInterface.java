6d9fac2deee9d53854026e70ec1037fe3198b17d
amahdyy <119822536+amahdyy@users.noreply.github.com>
2023-05-23 00:09:41 +0300

    Update AutheticationServicesInterface.java

--- src/main/java/elakeel/services/AutheticationServicesInterface.java
+++ src/main/java/elakeel/services/AutheticationServicesInterface.java
@@ -1,3 +1,10 @@
 package elakeel.services;
 
 import elakeel.ejbs.User;
+
+public interface AutheticationServicesInterface {
+
+User signUp(User user);
+	
+User login(String username, String password);
+}
