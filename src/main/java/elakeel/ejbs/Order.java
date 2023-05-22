57768a0ca06039fb363010f067f4d287a0ffcfea
mohamedsalama1020 <94934576+mohamedsalama1020@users.noreply.github.com>
2023-05-22 23:58:43 +0300

    Update Order.java

--- src/main/java/elakeel/ejbs/Order.java
+++ src/main/java/elakeel/ejbs/Order.java
@@ -16,3 +16,110 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+@Entity
+@Table(name = "orders")
+public class Order implements Serializable {
+	
+	 @ManyToOne
+	    private Customer customer;
+
+	    public Customer getCustomer() {
+		return customer;
+	}
+
+
+		public void setCustomer(Customer customer) {
+	        this.customer = customer;
+	        if (customer != null) {
+	            customer.getOrders().add(this);
+	        }
+	    }
+	
+	
+    public Order() {
+		super();
+	}
+
+	public Order(Long id, List<OrderItem> items, double totalPrice, Runner runner, Restaurant restaurant,
+			OrderStatus status) {
+		super();
+		this.id = id;
+		this.items = items;
+		this.totalPrice = totalPrice;
+		this.runner = runner;
+		this.restaurant = restaurant;
+		this.status = status;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public List<OrderItem> getItems() {
+		return items;
+	}
+
+	public void setItems(List<OrderItem> items) {
+		this.items = items;
+	}
+
+	public double getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(double totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+	public Runner getRunner() {
+		return runner;
+	}
+
+	public void setRunner(Runner runner) {
+		this.runner = runner;
+	}
+
+	public Restaurant getRestaurant() {
+		return restaurant;
+	}
+
+	public void setRestaurant(Restaurant restaurant) {
+		this.restaurant = restaurant;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> items;
+
+    @Column(nullable = false)
+    private double totalPrice;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "runner_id")
+    private Runner runner;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    
+}
