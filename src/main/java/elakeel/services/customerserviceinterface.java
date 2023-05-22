14257aea8e00225e155501988de6bde6abab3201
amahdyy <119822536+amahdyy@users.noreply.github.com>
2023-05-23 00:10:27 +0300

    Update customerserviceinterface.java

--- src/main/java/elakeel/services/customerserviceinterface.java
+++ src/main/java/elakeel/services/customerserviceinterface.java
@@ -4,3 +4,10 @@ import java.util.List;
 
 import elakeel.ejbs.Order;
 import elakeel.ejbs.OrderItem;
+
+public interface customerserviceinterface {
+	
+	 Order createOrder(Long customerId, Long restaurantId, List<OrderItem> items);
+	 List<Order> getOrdersByCustomerId(Long customerId);
+}
+
