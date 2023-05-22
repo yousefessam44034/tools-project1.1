de9e96ebff73bf67258e0d1663c14a80acce3e29
AmroAdel1 <119890404+AmroAdel1@users.noreply.github.com>
2023-05-23 00:12:54 +0300

    Update testapi.java

--- src/main/java/elakeel/ejbs/testapi.java
+++ src/main/java/elakeel/ejbs/testapi.java
@@ -2,3 +2,11 @@ package elakeel.ejbs;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+
+@Path("/test")
+public class testapi {
+	@GET
+	public String testapi() {
+	return "This is el akeel test";
+	}
+}
