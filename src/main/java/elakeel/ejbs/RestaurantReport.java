9addc8d58276a462f0d6584a9d597b41fcb5afc5
AmroAdel1 <119890404+AmroAdel1@users.noreply.github.com>
2023-05-23 00:08:39 +0300

    Update RestaurantReport.java

--- src/main/java/elakeel/ejbs/RestaurantReport.java
+++ src/main/java/elakeel/ejbs/RestaurantReport.java
@@ -1,1 +1,46 @@
 package elakeel.ejbs;
+
+public class RestaurantReport {
+    public RestaurantReport() {
+		super();
+	}
+	public RestaurantReport(Restaurant restaurant, double totalEarnings, int completedOrdersCount,
+			int canceledOrdersCount) {
+		super();
+		this.restaurant = restaurant;
+		this.totalEarnings = totalEarnings;
+		this.completedOrdersCount = completedOrdersCount;
+		this.canceledOrdersCount = canceledOrdersCount;
+	}
+	public Restaurant getRestaurant() {
+		return restaurant;
+	}
+	public void setRestaurant(Restaurant restaurant) {
+		this.restaurant = restaurant;
+	}
+	public double getTotalEarnings() {
+		return totalEarnings;
+	}
+	public void setTotalEarnings(double totalEarnings) {
+		this.totalEarnings = totalEarnings;
+	}
+	public int getCompletedOrdersCount() {
+		return completedOrdersCount;
+	}
+	public void setCompletedOrdersCount(int completedOrdersCount) {
+		this.completedOrdersCount = completedOrdersCount;
+	}
+	public int getCanceledOrdersCount() {
+		return canceledOrdersCount;
+	}
+	public void setCanceledOrdersCount(int canceledOrdersCount) {
+		this.canceledOrdersCount = canceledOrdersCount;
+	}
+	private Restaurant restaurant;
+    private double totalEarnings;
+    private int completedOrdersCount;
+    private int canceledOrdersCount;
+    
+}
+
+}
