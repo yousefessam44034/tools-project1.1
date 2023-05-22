15f4152f5bb36466bd572d0dfb56e34d65fc6171
AmroAdel1 <119890404+AmroAdel1@users.noreply.github.com>
2023-05-23 00:09:52 +0300

    Update Runner.java

--- src/main/java/elakeel/ejbs/Runner.java
+++ src/main/java/elakeel/ejbs/Runner.java
@@ -10,3 +10,41 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+@Entity
+@Table(name = "runners")
+public class Runner extends User implements Serializable {
+    public Runner() {
+		super();
+	}
+
+	public Runner(double deliveryFees, RunnerStatus status) {
+		super();
+		this.deliveryFees = deliveryFees;
+		this.status = status;
+	}
+
+	public double getDeliveryFees() {
+		return deliveryFees;
+	}
+
+	public void setDeliveryFees(double deliveryFees) {
+		this.deliveryFees = deliveryFees;
+	}
+
+	public RunnerStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(RunnerStatus status) {
+		this.status = status;
+	}
+
+	@Column(nullable = false)
+    private double deliveryFees;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private RunnerStatus status;
+ 
+}
