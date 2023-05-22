c763a52716ecc0cc57c2cf3bf45319b909d1b881
mohamedsalama1020 <94934576+mohamedsalama1020@users.noreply.github.com>
2023-05-23 00:02:02 +0300

    Update Restaurant.java

--- src/main/java/elakeel/ejbs/Restaurant.java
+++ src/main/java/elakeel/ejbs/Restaurant.java
@@ -15,3 +15,81 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "restaurants")
+public class Restaurant implements Serializable {
+	
+	@Column(nullable = false)
+	private double deliveryFees;
+	
+	public Restaurant() {
+		super();
+	}
+
+	public Restaurant(double deliveryFees, Long id, String name, RestaurantOwner owner, List<Meal> menu) {
+		super();
+		this.deliveryFees = deliveryFees;
+		this.id = id;
+		this.name = name;
+		this.owner = owner;
+		this.menu = menu;
+	}
+
+	public double getDeliveryFees() {
+		return deliveryFees;
+	}
+
+	public void setDeliveryFees(double deliveryFees) {
+		this.deliveryFees = deliveryFees;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public RestaurantOwner getOwner() {
+		return owner;
+	}
+
+	public void setOwner(RestaurantOwner owner) {
+		this.owner = owner;
+	}
+
+	public List<Meal> getMenu() {
+		return menu;
+	}
+
+	public void setMenu(List<Meal> menu) {
+		this.menu = menu;
+	}
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_id")
+    private RestaurantOwner owner;
+
+    @OneToMany(mappedBy = "restaurant", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Meal> menu;
+
+    
+}
