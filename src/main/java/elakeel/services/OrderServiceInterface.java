be7166a089c1b4fec0d9a8ffd52a4b08eb61deff
amahdyy <119822536+amahdyy@users.noreply.github.com>
2023-05-23 00:12:04 +0300

    Update OrderServiceInterface.java

--- src/main/java/elakeel/services/OrderServiceInterface.java
+++ src/main/java/elakeel/services/OrderServiceInterface.java
@@ -6,3 +6,16 @@ import elakeel.ejbs.Order;
 import elakeel.ejbs.OrderItem;
 import elakeel.ejbs.User;
 
+
+public interface OrderServiceInterface {
+	Order getOrderById(Long orderId);
+	
+	User selectRandomAvailableRunner();
+	
+	Order createOrder(Order order);
+	
+	List<Order> getOrdersByCustomerId(Long customerId);
+	
+	void editOrder(Order order, List<OrderItem> newItems);
+
+}
